0bd702ab9cd0b5d176664cdb2226efdae9bf7579
Waltika <walter.wartenweiler@gmail.com>
2025-04-18 19:27:04 +0200

    Forcing publishing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 console.log('Annotation app.js: Script started');
 
 
+
 // Simple UUID generator for anonymous users
 function generateUUID() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
