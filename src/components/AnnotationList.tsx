d05ef980221d3aa10daa280c48bbf6fc3f350919
Waltika <walter.wartenweiler@gmail.com>
2025-04-30 23:44:25 +0200

    Small fix

--- src/components/AnnotationList.tsx
+++ src/components/AnnotationList.tsx
@@ -209,7 +209,7 @@ export const AnnotationList: React.FC<AnnotationListProps> = ({ annotations, pro
                             <button
                                 onClick={() => handleCopyLink(showShareModal)}
                                 className="share-button-social share-copy"
-                                title="Copy Link"
+                                title="Copy Message"
                             >
                                 <svg viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
