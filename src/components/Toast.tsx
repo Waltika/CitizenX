b5113ba58b9602dc4b32bd4662d777edde3846b5
Waltika <walter.wartenweiler@gmail.com>
2025-05-05 20:51:32 +0200

    Replacing popup after sharing with a Toast

---
+++ src/components/Toast.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Toast.css';
+
+interface ToastProps {
+    message: string;
+    isVisible: boolean;
+}
+
+export const Toast: React.FC<ToastProps> = ({ message, isVisible }) => {
+    if (!isVisible) return null;
+
+    return (
+        <div className="toast">
+            {message}
+        </div>
+    );
+};
